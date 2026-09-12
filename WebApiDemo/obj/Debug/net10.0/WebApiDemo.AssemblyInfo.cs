b49ca0f1395755037b8821c552a5059744600205
danielvazquezpalestino <daniel.vazquezpalestino@gmail.com>
2026-09-12 00:52:07 -0600

    add
    agreo configuracionesd npm

--- WebApiDemo/obj/Debug/net10.0/WebApiDemo.AssemblyInfo.cs
+++ WebApiDemo/obj/Debug/net10.0/WebApiDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+796884ecc543171154e43c3d372a0594b7897f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
